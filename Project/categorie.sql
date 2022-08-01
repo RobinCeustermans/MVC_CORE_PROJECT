a260f5c57bce05c88e3fbc84205411dc547b93ac
RobinCeustermans <robin.ceustermans@outlook.com>
2022-08-01 16:59:50 +0200

    init

---
+++ Project/categorie.sql
@@ -0,0 +1,14 @@
+SET IDENTITY_INSERT dbo.Categorie ON
+
+insert Into dbo.Categorie(CategorieID,Beschrijving)
+values
+(1, 'lichtreclame'),
+(2,'vlaggen'),
+(3, 'militaria'),
+(4, 'verkoopautomaat'),
+(5, 'speelgoed'),
+(6, 'reclameborden'),
+(7, 'Elektronische Apparatuur'),
+(8, 'motoren'),
+(9, 'blikken, dozen'),
+(10, 'overig')
